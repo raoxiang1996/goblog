aa3a76919289a2d12b3f8fc5ca7bb918638b4d57
raoxiang <raoxiang1996@gmail.com>
2020-09-22 19:47:53 +0800

    增加上传

---
+++ api/v1/upload.go
@@ -0,0 +1,25 @@
+package v1
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"goblog/model"
+	"goblog/utils/errmsg"
+)
+
+func UpLoad(c *gin.Context) {
+	file, fileHeader, _ := c.Request.FormFile("file")
+
+	fileSize := fileHeader.Size
+
+	url, code := model.UpLoadFile(file, fileSize)
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"message": errmsg.GetErrMsg(code),
+		"url":     url,
+	})
+
+}
